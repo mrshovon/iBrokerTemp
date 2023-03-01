f8dd8329d00ea4cca0b70b1776f61fa41cedd3bf
mrshovon <mushfiq.rh6115@gmail.com>
2023-03-01 22:13:26 +0600

    tableclrchng

--- Template_4/js/main.js
+++ Template_4/js/main.js
@@ -58,7 +58,8 @@
         nav : false
     });
 
-
+    //Chart Default color for xaxix and yaxis
+    Chart.defaults.color = '#ffffff';
     // Worldwide Sales Chart
     var ctx1 = $("#worldwide-sales").get(0).getContext("2d");
     var myChart1 = new Chart(ctx1, {
